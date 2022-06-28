07d6efc52c49c77d092acd9b121af973aba7528d
ShashwikaMekala <91323437+ShashwikaMekala@users.noreply.github.com>
2022-06-28 15:43:49 +0530

    Added cypress framework

---
+++ cypress/e2e/sample.cy.js
@@ -0,0 +1,16 @@
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(true)
+    })
+  })
+  describe('My First Test', () => {
+    it('Visits the Kitchen Sink', () => {
+      cy.visit('https://example.cypress.io')
+      cy.contains('type').click()
+    })
+  })
+  describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(false)
+    })
+  })
